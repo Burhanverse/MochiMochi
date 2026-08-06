8d80baf1d5b5b83add17a6aedfd498eaef6a74f5
Burhanverse <contact@burhanverse.eu.org>
2026-08-06 16:51:34 +0530

    chore: remove unused assertEquals import in BotTokenManagerTest
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- Android/app/src/test/java/com/kawai/mochi/BotTokenManagerTest.java
+++ Android/app/src/test/java/com/kawai/mochi/BotTokenManagerTest.java
@@ -2,7 +2,6 @@ package com.kawai.mochi;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
